b865d86338ddea8e76ae351027184a01833cc265
Nam <vn5753378@gmail.com>
2026-03-25 10:28:01 +0700

    Create Launcher.java

---
+++ bidding-client/src/main/java/com/uet/client/Launcher.java
@@ -0,0 +1,8 @@
+package com.uet.client;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Gọi hàm main của ClientApp để khởi chạy JavaFX
+        ClientApp.main(args);
+    }
+}
