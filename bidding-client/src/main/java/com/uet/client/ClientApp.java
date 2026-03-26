ac214f37bd453f4d237fc8b6d321da2fe19b696a
Nam <vn5753378@gmail.com>
2026-03-26 13:48:59 +0700

    Update ClientApp.java

--- bidding-client/src/main/java/com/uet/client/ClientApp.java
+++ bidding-client/src/main/java/com/uet/client/ClientApp.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 
@@ -13,9 +14,11 @@ public class ClientApp extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            Image icon = new Image(getClass().getResourceAsStream("/photo/icon_app.png"));
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login_view.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root);
+            stage.getIcons().add(icon);
             stage.setTitle("Hệ thống Đấu giá UET - Đăng nhập");
             stage.setScene(scene);
             stage.show();
