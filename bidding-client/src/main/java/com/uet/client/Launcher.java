759ceadb245401e682d36448af6dc718cffa6444
nguyentienminhnhat-25021923 <25021923@vnu.edu.vn>
2026-03-26 21:15:08 +0700

    Update Launcher.java

--- bidding-client/src/main/java/com/uet/client/Launcher.java
+++ bidding-client/src/main/java/com/uet/client/Launcher.java
@@ -2,7 +2,6 @@ package com.uet.client;
 
 public class Launcher {
     public static void main(String[] args) {
-        // Gọi hàm main của ClientApp để khởi chạy JavaFX
         ClientApp.main(args);
     }
 }
